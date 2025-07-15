4fcd7c5b137c3a60194e5286708a262cf747e7da
hossiiiw <hossiiw.0933@gmail.com>
2025-07-15 12:30:13 +0330

    add prisma

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,11 @@
+// import { prisma } from '@/lib/prisma';
 import ProductListView from '@/modules/product/views/ProductListView';
+import { Prisma, PrismaClient } from '@prisma/client';
 import React from 'react';
 
-function Products() {
+async function Products() {
+  const prisma = await new PrismaClient().Product;
+
   return (
     <div className="flex flex-col">
       <h1 className="font-bold text-xl mx-auto my-8">product page</h1>
